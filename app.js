c3285f0cf93c2d486445562b0558d25dbeeffc84
Zarema Tassyrova <zarema.tsr@gmail.com>
2022-04-06 14:59:13 -0400

    rename

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
+
 //Create a logo on the top left corner
 //Create two input windows that reflect current score and best score
 //Create a line of rule
 //create a menu drop-out button
-// I need to create a 4x4 square 
-
-
+// I need to create a 4x4 square with 16 different squares
+//
 //
